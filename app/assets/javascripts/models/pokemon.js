0b9b2d74a6a37c365c41c1b27dd077bc6eef1717
App Academy Student <student@appacademy.io>
2015-08-03 17:02:34 -0700

    add toy model and collection

--- app/assets/javascripts/models/pokemon.js
+++ app/assets/javascripts/models/pokemon.js
@@ -1,3 +1,20 @@
 Pokedex.Models.Pokemon = Backbone.Model.extend ({
   urlRoot: "/pokemon",
+
+  toys: function() {
+    if (typeof this._toys === "undefined") {
+      this._toys = new Pokedex.Collections.Toys([], { pokemon: this });
+    }
+    return this._toys;
+  },
+
+  parse: function(payload) {
+    if (payload.toys) {
+      this.toys().set(payload.toys);
+      delete payload.toys;
+    }
+    return payload;
+  }
+
+
 })
